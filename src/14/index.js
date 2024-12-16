85550edda3bf0550f80708ca3de522415be9977a
Jan Korona <jan@withlocals.com>
2024-12-16 14:25:03 +0100

    day 15 wip

--- src/14/index.js
+++ src/14/index.js
@@ -1,21 +1,6 @@
 // https://adventofcode.com/2024/day/14
 const { readFile } = require("../io");
 
-const data = `
-p=0,4 v=3,-3
-p=6,3 v=-1,-3
-p=10,3 v=-1,2
-p=2,0 v=2,-1
-p=0,0 v=1,3
-p=3,0 v=-2,-2
-p=7,6 v=-1,-3
-p=3,0 v=-1,-2
-p=9,3 v=2,3
-p=7,3 v=-1,2
-p=2,4 v=2,-3
-p=9,5 v=-3,-3
-`.trim();
-
 const parse = (input) =>
   input
     .split("\n")
