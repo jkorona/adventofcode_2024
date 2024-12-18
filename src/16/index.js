66e566915e36d12ec8c8f15e888a45a2ca45c8b9
Jan Korona <jan@withlocals.com>
2024-12-18 13:14:45 +0100

    day 16

--- src/16/index.js
+++ src/16/index.js
@@ -2,26 +2,6 @@
 const { readFile } = require("../io");
 const { sum } = require("../utils");
 
-const data = `
-#################
-#...#...#...#..E#
-#.#.#.#.#.#.#.#.#
-#.#.#.#...#...#.#
-#.#.#.#.###.#.#.#
-#...#.#.#.....#.#
-#.#.#.#.#.#####.#
-#.#...#.#.#.....#
-#.#.#####.#.###.#
-#.#.#.......#...#
-#.#.###.#####.###
-#.#.#...#.....#.#
-#.#.#.#####.###.#
-#.#.#.........#.#
-#.#.#.#########.#
-#S#.............#
-#################
-`.trim();
-
 const parse = (input) => {
   let start, end;
   const maze = input.split("\n").map((row, rowIndex) => {
@@ -55,60 +35,71 @@ class PriorityQueue {
   }
 }
 
-const dijkstra = (maze, start, end) => {
-  const directions = [
-    [0, 1, 0],  // right
-    [1, 0, 1],  // down
-    [0, -1, 2], // left
-    [-1, 0, 3], // up
-  ];
+const key = (point) => point.join("|");
+const directions = [
+  [0, 1, 0], // right
+  [1, 0, 1], // down
+  [0, -1, 2], // left
+  [-1, 0, 3], // up
+];
 
+const dijkstra = (maze, start, end) => {
+  const paths = [];
   const pq = new PriorityQueue();
 
   const cost = Array.from({ length: maze.length }, () =>
     Array.from({ length: maze[0].length }, () => Array(4).fill(Infinity))
   );
 
-  for (let i = 0; i < 4; i++) {
-    pq.enqueue([start[0], start[1], i, i === 0 ? 1 : 1000], 0);
-    cost[start[0]][start[1]][i] = 0;
-  }
+  pq.enqueue([start[0], start[1], 0, 0, [key(start)]], 0);
 
   while (!pq.isEmpty()) {
-    const [x, y, dir, currentCost] = pq.dequeue();
+    const [x, y, dir, currentCost, path] = pq.dequeue();
 
     if (x === end[0] && y === end[1]) {
-      return currentCost;
+      paths.push({ cost: currentCost, path });
     }
 
     for (const [dx, dy, newDir] of directions) {
       const newX = x + dx;
       const newY = y + dy;
 
-      if (maze[newX][newY] !== '#') {
-        const additionalCost = dir === newDir ? 1 : 1 + 1000;
+      if (maze[newX][newY] !== "#") {
+        const additionalCost = dir === newDir ? 1 : 1001;
         const newCost = currentCost + additionalCost;
 
-        if (newCost < cost[newX][newY][newDir]) {
+        if (newCost <= cost[newX][newY][newDir]) {
           cost[newX][newY][newDir] = newCost;
-          pq.enqueue([newX, newY, newDir, newCost], newCost);
+          pq.enqueue(
+            [newX, newY, newDir, newCost, [...path, key([newX, newY])]],
+            newCost
+          );
         }
       }
     }
   }
 
-  return -1;
-  
+  return paths;
 };
 
 const firstTask = (input) => {
   const [maze, start, end] = parse(input);
-  return dijkstra(maze, start, end);
+  const paths = dijkstra(maze, start, end);
+  return paths.at(0)?.cost;
 };
 
-const secondTask = (input) => 0;
+const secondTask = (input) => {
+  const [maze, start, end] = parse(input);
+  const paths = dijkstra(maze, start, end);
 
-// console.log(firstTask(data));
+  const sortedPaths = paths.sort((a, b) => a.score - b.score);
+  const minCost = sortedPaths[0].cost;
+
+  const uniqueTiles = sortedPaths
+    .filter(({ cost }) => cost === minCost)
+    .flatMap(({ path }) => path);
+  return new Set(uniqueTiles).size;
+};
+
 console.log(firstTask(readFile("./src/16/input")));
-console.log(secondTask(data));
-// console.log(secondTask(readFile("./src/16/input")))
+console.log(secondTask(readFile("./src/16/input")));
